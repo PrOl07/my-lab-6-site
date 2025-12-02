fec4cbb3211dc8c169f6e97a112e0af7206a1f57
PrOl07 <protasov.oleksandr@lll.kpi.ua>
2025-12-02 16:13:41 +0200

    Delete save.php

--- save.php
+++
@@ -1,9 +0,0 @@
-<?php
-$data = file_get_contents('php://input');
-if($data) {
-    file_put_contents('collapse_data.json', $data);
-    echo "Дані успішно збережено!";
-} else {
-    echo "Немає даних для збереження.";
-}
-?>
